8712c3146d95fb09ef6f922ef66b7c9cd929f85e
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-05-18 18:29:02 +0100

    Created method collectAnimals for zookeper

--- main.go
+++ main.go
@@ -23,3 +23,12 @@ func (cage Cage) addAnimal(animal Animal) Cage {
 	cage.Animals = append(cage.Animals, animal)
 	return cage
 }
+
+func (zookeeper Zookeeper) CollectAnimals(cages ...Cage) []Animal {
+	var allAnimals []Animal
+	for _, cage := range cages {
+		allAnimals = append(allAnimals, cage.Animals...)
+	}
+
+	return allAnimals
+}
