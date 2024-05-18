67338d2c5b33e613a20cb8dd26a2170a88757abe
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-05-18 19:36:06 +0100

    Function main - Zookeper collected animals, sorted animals and print results

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Meal string
 
 const (
@@ -22,28 +24,55 @@ type Cage struct {
 	Animals []Animal
 }
 
-// func addAnimal(animal Animal, cage Cage) Cage {
-// 	cage.Animals = append(cage.Animals, animal)
-// 	return cage
-// }
-
-func (cage Cage) addAnimal(animal Animal) Cage {
+func (cage *Cage) addAnimal(animal Animal) {
 	cage.Animals = append(cage.Animals, animal)
-	return cage
 }
 
-func (zookeeper Zookeeper) CollectAnimals(cages ...Cage) []Animal {
+func (zookeeper Zookeeper) CollectAnimals(animals ...Animal) []Animal {
 	var allAnimals []Animal
-	for _, cage := range cages {
-		allAnimals = append(allAnimals, cage.Animals...)
+	for _, animal := range animals {
+		allAnimals = append(allAnimals, animal)
 	}
 
 	return allAnimals
 }
 
-func main() {
-	lion := Animal{Name: "Lion", Voice: "RoarR"}
-	monkey := Animal{Name: "Monkey", Voice: "Ooh ooh aah aah"}
-	snake := Animal{Name: "Snake", Voice: "Shhhh"}
+func sortAnimals(animals []Animal) (cage1 Cage, cage2 Cage) {
+	for _, animal := range animals {
+		if animal.Meal == Meat {
+			cage1.addAnimal(animal)
+		} else if animal.Meal == Veggie {
+			cage2.addAnimal(animal)
+		}
+	}
 
+	return cage1, cage2
+}
+
+func printAnimals(cage Cage) {
+	for _, animal := range cage.Animals {
+		fmt.Printf("%s  ", animal.Name)
+	}
+}
+
+func main() {
+	lion := Animal{Name: "Lion", Voice: "RoarR", Meal: Meat}
+	monkey := Animal{Name: "Monkey", Voice: "Ooh ooh aah aah", Meal: Veggie}
+	snake := Animal{Name: "Snake", Voice: "Shhhh", Meal: Veggie}
+
+	zookeeper := Zookeeper{Name: "Alex", Age: 32}
+	allAnimals := zookeeper.CollectAnimals(lion, monkey, snake)
+
+	cage1, cage2 := sortAnimals(allAnimals)
+
+	fmt.Printf("Animals collected by the zookeeper: %s, he is %d years old", zookeeper.Name, zookeeper.Age)
+	println()
+	for _, animal := range allAnimals {
+		fmt.Printf("%s says %s\n", animal.Name, animal.Voice)
+	}
+
+	fmt.Printf("First cage: ")
+	printAnimals(cage1)
+	fmt.Printf("\nSecond cage: ")
+	printAnimals(cage2)
 }
