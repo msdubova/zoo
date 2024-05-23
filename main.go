cb2854e9e021a985be5c7b27da21b5fc261b6ba4
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-05-23 21:13:38 +0100

    Edition: changed methods CollectAnimals and PrintAnimals

--- main.go
+++ main.go
@@ -16,8 +16,10 @@ type Animal struct {
 }
 
 type Zookeeper struct {
-	Name string
-	Age  int
+	Name  string
+	Age   int
+	Cage1 Cage
+	Cage2 Cage
 }
 
 type Cage struct {
@@ -28,29 +30,32 @@ func (cage *Cage) addAnimal(animal Animal) {
 	cage.Animals = append(cage.Animals, animal)
 }
 
-func (zookeeper Zookeeper) CollectAnimals(animals ...Animal) []Animal {
-	var allAnimals []Animal
-	for _, animal := range animals {
-		allAnimals = append(allAnimals, animal)
-	}
+func (zookeeper *Zookeeper) CollectAnimals(animals ...Animal) {
 
-	return allAnimals
-}
-
-func sortAnimals(animals []Animal) (cage1 Cage, cage2 Cage) {
 	for _, animal := range animals {
 		if animal.Meal == Meat {
-			cage1.addAnimal(animal)
+			zookeeper.Cage1.addAnimal(animal)
 		} else if animal.Meal == Veggie {
-			cage2.addAnimal(animal)
+			zookeeper.Cage2.addAnimal(animal)
 		}
 	}
 
-	return cage1, cage2
 }
 
-func printAnimals(cage Cage) {
-	for _, animal := range cage.Animals {
+func (zookeeper Zookeeper) printAnimals() {
+	fmt.Printf("Animals collected by the zookeeper: %s, he is %d years old\n", zookeeper.Name, zookeeper.Age)
+
+	for _, animal := range append(zookeeper.Cage1.Animals, zookeeper.Cage2.Animals...) {
+		fmt.Printf("%s says %s\n", animal.Name, animal.Voice)
+	}
+
+	fmt.Printf("First cage: ")
+	for _, animal := range zookeeper.Cage1.Animals {
+		fmt.Printf("%s  ", animal.Name)
+	}
+
+	fmt.Printf("\nSecond cage: ")
+	for _, animal := range zookeeper.Cage2.Animals {
 		fmt.Printf("%s  ", animal.Name)
 	}
 }
@@ -59,20 +64,8 @@ func main() {
 	lion := Animal{Name: "Lion", Voice: "RoarR", Meal: Meat}
 	monkey := Animal{Name: "Monkey", Voice: "Ooh ooh aah aah", Meal: Veggie}
 	snake := Animal{Name: "Snake", Voice: "Shhhh", Meal: Veggie}
-
 	zookeeper := Zookeeper{Name: "Alex", Age: 32}
-	allAnimals := zookeeper.CollectAnimals(lion, monkey, snake)
 
-	cage1, cage2 := sortAnimals(allAnimals)
-
-	fmt.Printf("Animals collected by the zookeeper: %s, he is %d years old", zookeeper.Name, zookeeper.Age)
-	println()
-	for _, animal := range allAnimals {
-		fmt.Printf("%s says %s\n", animal.Name, animal.Voice)
-	}
-
-	fmt.Printf("First cage: ")
-	printAnimals(cage1)
-	fmt.Printf("\nSecond cage: ")
-	printAnimals(cage2)
+	zookeeper.CollectAnimals(lion, monkey, snake)
+	zookeeper.printAnimals()
 }
